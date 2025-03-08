ad5e70e8df580dec685a4ef9541d94e252fc0b89
diivvuu <divyanshubawa9@gmail.com>
2025-03-08 19:53:54 +0530

    app setup, shadcn, header completed

---
+++ src/app/_components/Header.tsx
@@ -0,0 +1,48 @@
+'use client';
+import Image from 'next/image';
+import logo from '@/app/assets/nutriEconomy.png';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+
+const Header = () => {
+  const router = useRouter();
+  return (
+    <div className="relative w-full bg-[#1F4F46] flex items-center p-8">
+      <div
+        className="flex justify-start cursor-pointer"
+        onClick={() => {
+          router.push('/');
+        }}
+      >
+        <Image src={logo} alt="logo" />
+      </div>
+      <div className="absolute left-1/2 -translate-x-1/2 flex items-center text-white text-2xl gap-x-12">
+        <div className="cursor-pointer hover:scale-105 transition-all ease-in-out duration-300">
+          Home
+        </div>
+        <div className="cursor-pointer hover:scale-105 transition-all ease-in-out duration-300">
+          Diet
+        </div>
+        <div className="cursor-pointer hover:scale-105 transition-all ease-in-out duration-300">
+          Product
+        </div>
+        <div className="cursor-pointer hover:scale-105 transition-all ease-in-out duration-300">
+          Health
+        </div>
+        <div className="cursor-pointer hover:scale-105 transition-all ease-in-out duration-300">
+          Sales
+        </div>
+      </div>
+      <div className="flex justify-end ml-auto">
+        <Button
+          className="bg-transparent text-white text-2xl border-2 border-white rounded-full px-6 py-5"
+          variant={'outline'}
+        >
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/app/_components/Section1.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Section1 = () => {
+  return <div className="h-screen w-full">nice</div>;
+};
+
+export default Section1;
